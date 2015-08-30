d9425d6ff90cdd74a361dae86009c15e14d43629
eamonpenland <eamon.penland@gmail.com>
2015-08-30 09:47:48 -0400

    fixed my loan pagestill not showing data

---
+++ www/js/myloans/myloans.routes.js
@@ -0,0 +1,18 @@
+(function(){
+  'use strict';
+  angular.module('app')
+    .config(configure);
+
+  function configure($stateProvider){
+    $stateProvider
+      .state('app.myloans', {
+      url: '/myloans',
+      views: {
+        'menuContent': {
+          templateUrl: 'js/myloans/myloans.html',
+          controller: 'MyLoansCtrl'
+        }
+      }
+    });
+  }
+})();
